6fc7142131b84bd34c34d5a64b7e080cd88f4362
makotty <hal.m.90215@gmail.com>
2022-09-15 00:25:53 +0900

    Update : specified-position page

--- src/pages/specified-position.tsx
+++ src/pages/specified-position.tsx
@@ -1,3 +1,13 @@
-const SpecifiedPosition = () => <>SpecifiedPositionn</>
+import Layout from '../components/Layout'
+
+const SpecifiedPosition = () => (
+  <Layout>
+    <div>
+      <h1 className="text-2xl font-serif text-white">
+        どこから見える星座を表示しますか？
+      </h1>
+    </div>
+  </Layout>
+)
 
 export default SpecifiedPosition
