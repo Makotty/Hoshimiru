16dc35892c103c0e91205b26f9686e0ce7ae8df8
makotty <hal.m.90215@gmail.com>
2022-09-14 17:31:46 +0900

    Fix : BackGround Color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 export const Header = () => (
-  <header className="bg-gray-800 py-5">
+  <header className="bg-gray-900 py-5">
     <h1 className="ml-6  text-yellow-200 text-2xl ">Hoshimiru.</h1>
   </header>
 )
