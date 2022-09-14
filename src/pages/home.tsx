cc02158ca7c69374bdb877d34615815d593ecbf5
makotty <hal.m.90215@gmail.com>
2022-09-15 00:25:41 +0900

    Update : home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout'
+
+const Home = () => (
+  <Layout>
+    <div>
+      <h1 className="text-2xl font-serif text-white">
+        どこから見える星を表示しますか？
+      </h1>
+    </div>
+  </Layout>
+)
+
+export default Home
