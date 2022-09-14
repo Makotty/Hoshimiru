bf619d3008e7f94738081f8095ca4e1bed6433f9
makotty <hal.m.90215@gmail.com>
2022-09-15 03:27:51 +0900

    Update : Home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,10 +2,8 @@ import Layout from '../components/Layout'
 
 const Home = () => (
   <Layout>
-    <div>
-      <h1 className="text-2xl font-serif text-white">
-        どこから見える星を表示しますか？
-      </h1>
+    <div className="flex justify-center ">
+      <h1 className="text-2xl font-serif text-white">Hoshimiru</h1>
     </div>
   </Layout>
 )
