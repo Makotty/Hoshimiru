272adce8d5037d15266ecfe56bcc189bb0e1d1d8
makotty <hal.m.90215@gmail.com>
2022-09-15 00:25:13 +0900

    現在地取得

--- src/pages/current-location.tsx
+++ src/pages/current-location.tsx
@@ -1,3 +1,42 @@
-const CurrentLocation = () => <>CurrentLocation</>
+import { useEffect, useState } from 'react'
+import Layout from '../components/Layout'
+
+const CurrentLocation = () => {
+  const [getLat, setGetLat] = useState<number>()
+  const [getLng, setGetLng] = useState<number>()
+
+  if (!navigator.geolocation) {
+    alert('あなたの端末では現在地を取得することができません')
+  }
+
+  const GetLocation = (position: GeolocationPosition) => {
+    setGetLat(position.coords.latitude)
+    setGetLng(position.coords.longitude)
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      GetLocation,
+      () => {
+        console.log('error')
+      },
+      { enableHighAccuracy: true }
+    )
+
+    
+  }, [])
+
+  return (
+    <Layout>
+      <div>
+        <h1 className="text-2xl font-serif text-white">
+          現在地から見える星を表示します。
+        </h1>
+        <p className="text-white">{getLat}</p>
+        <p className="text-white">{getLng}</p>
+      </div>
+    </Layout>
+  )
+}
 
 export default CurrentLocation
