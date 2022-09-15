4a68387e2554b9a08a577ead89adfbde743d2088
makotty <hal.m.90215@gmail.com>
2022-09-16 00:09:18 +0900

    星座の取得と出力

--- src/pages/current-location.tsx
+++ src/pages/current-location.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Layout from '../components/Layout'
+import { SignResponseTypes } from '../types/SignResponse.'
 
 const CurrentLocation = () => {
   const [getLat, setGetLat] = useState<number>(35.18028)
   const [getLng, setGetLng] = useState<number>(136.90667)
+
+  const [signRes, setSignRes] = useState<SignResponseTypes>()
 
   if (!navigator.geolocation) {
     alert('あなたの端末では現在地を取得することができません')
@@ -36,8 +39,8 @@ const CurrentLocation = () => {
       .get(
         `https://livlog.xyz/hoshimiru/constellation?lat=${getLat}&lng=${getLng}&date=${date}&hour=${hour}&min=${min}`
       )
-      .then((response) => {
-        console.log(response)
+      .then((response: SignResponseTypes) => {
+        setSignRes(response)
       })
       .catch((error) => {
         console.log(error)
@@ -45,17 +48,63 @@ const CurrentLocation = () => {
   }, [])
 
   return (
-    <>
-      <Layout>
-        <div className="text-center">
-          <h2 className="my-8 text-xl font-serif text-white">
-            現在地から見える星座を表示します。
-          </h2>
-          <p className="text-white">緯度 : {getLat}</p>
-          <p className="text-white">軽度 : {getLng}</p>
-        </div>
-      </Layout>
-    </>
+    <Layout>
+      <div className="text-center flex justify-center items-center flex-col">
+        <h2 className="my-8 text-xl font-serif text-white">
+          現在地から見える星座を表示します。
+        </h2>
+        <p className="text-white">緯度 : {getLat}</p>
+        <p className="text-white">軽度 : {getLng}</p>
+
+        {signRes &&
+          signRes.data.result.map((data) => (
+            <div
+              className="md:w-2/4 w-4/5 p-4 text-slate-800 my-6 bg-gray-100 max-w-3xl rounded"
+              key={data.id}
+            >
+              <div className="flex">
+                <p>見える高さ：{data.altitude}</p>｜
+                <p>高度：{data.altitudeNum}度</p>
+              </div>
+              <div className="flex">
+                <p>方角：{data.direction}</p>｜
+                <p>方位角：{data.directionNum}°</p>
+              </div>
+
+              <figure className="flex flex-col items-center md:flex-row">
+                <img
+                  className="w-32 md:w-1/5 h-auto m-3"
+                  src={data.starIcon}
+                  alt={`${data.jpName}のアイコン`}
+                />
+                <figcaption className="flex items-start flex-col p-2">
+                  <span className="text-2xl mb-1">
+                    {data.jpName}
+                    <span className="ml-3 text-base text-slate-400">
+                      {data.enName}
+                    </span>
+                  </span>
+                  <span className="text-left">{data.content}</span>
+                </figcaption>
+              </figure>
+
+              <p className="text-sm text-left p-4">
+                ストーリー
+                <br />
+                {data.origin}
+              </p>
+
+              <figure className="flex justify-center">
+                <img
+                  // className="w-full h-auto"
+                  src={data.starImage}
+                  alt={`${data.jpName}のアイコン`}
+                />
+              </figure>
+            </div>
+          ))}
+      </div>
+    </Layout>
   )
 }
 
--- src/pages/specified-position.tsx
+++ src/pages/specified-position.tsx
@@ -2,6 +2,7 @@ import axios from 'axios'
 import { useState } from 'react'
 import Geocode from 'react-geocode'
 import Layout from '../components/Layout'
+import { SignResponseTypes } from '../types/SignResponse.'
 
 type GeocodeResponse = {
   results: [{ geometry: { location: { lat: number; lng: number } } }]
@@ -9,6 +10,8 @@ type GeocodeResponse = {
 
 const SpecifiedPosition = () => {
   const [place, setPlace] = useState<string>('HAL名古屋')
+
+  const [signRes, setSignRes] = useState<SignResponseTypes>()
 
   const newDate = new Date()
   const date = `${newDate.getFullYear()}-${
@@ -22,8 +25,8 @@ const SpecifiedPosition = () => {
       .get(
         `https://livlog.xyz/hoshimiru/constellation?lat=${lat}&lng=${lng}&date=${date}&hour=${hour}&min=${min}`
       )
-      .then((res) => {
-        console.log(res)
+      .then((response) => {
+        setSignRes(response)
       })
       .catch((error) => {
         console.log(error)
@@ -46,26 +49,76 @@ const SpecifiedPosition = () => {
 
   return (
     <Layout>
-      <div className="text-center">
+      <div className="text-center flex justify-center items-center flex-col">
         <h2 className="my-8 text-xl font-serif text-white">
           どこから見える星座を表示しますか？
         </h2>
 
-        <input
-          className="w-3/5 md:w-1/5 p-2 rounded-md"
-          type="text"
-          onChange={(e) => {
-            setPlace(e.target.value)
-          }}
-        />
+        <div className="flex justify-center items-center w-9/12 md:w-2/4 max-w-md">
+          <input
+            className="w-4/5 p-2 rounded-md"
+            type="text"
+            onChange={(e) => {
+              setPlace(e.target.value)
+            }}
+          />
 
-        <button
-          className="ml-4 px-4 py-2 bg-gray-400 hover:bg-gray-200 rounded-md transition"
-          type="button"
-          onClick={Geocoder}
-        >
-          表示
-        </button>
+          <button
+            className="w-1/5 ml-2 p-2  bg-gray-400 hover:bg-gray-200 rounded-md transition"
+            type="button"
+            onClick={Geocoder}
+          >
+            表示
+          </button>
+        </div>
+
+        {signRes &&
+          signRes.data.result.map((data) => (
+            <div
+              className="md:w-2/4 w-4/5 p-4 text-slate-800 my-6 bg-gray-100 max-w-3xl rounded"
+              key={data.id}
+            >
+              <div className="flex">
+                <p>見える高さ：{data.altitude}</p>｜
+                <p>高度：{data.altitudeNum}度</p>
+              </div>
+              <div className="flex">
+                <p>方角：{data.direction}</p>｜
+                <p>方位角：{data.directionNum}°</p>
+              </div>
+
+              <figure className="flex flex-col items-center md:flex-row">
+                <img
+                  className="w-1/5 h-auto m-3"
+                  src={data.starIcon}
+                  alt={`${data.jpName}のアイコン`}
+                />
+                <figcaption className="flex items-start flex-col p-2">
+                  <span className="text-2xl">
+                    {data.jpName}
+                    <span className="ml-3 text-base text-slate-400">
+                      {data.enName}
+                    </span>
+                  </span>
+                  <span className="text-left">{data.content}</span>
+                </figcaption>
+              </figure>
+
+              <p className="text-sm text-left p-4">
+                ストーリー
+                <br />
+                {data.origin}
+              </p>
+
+              <figure className="flex justify-center">
+                <img
+                  // className="w-full h-auto"
+                  src={data.starImage}
+                  alt={`${data.jpName}のアイコン`}
+                />
+              </figure>
+            </div>
+          ))}
       </div>
     </Layout>
   )
